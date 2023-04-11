53710110a9c93740d6065b13fe34843eae458330
Mariam Gasparian <m.gasparian11@gmail.com>
2023-04-11 18:58:27 +0400

    feat: add business logic for UserDetailsComponent

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from 'src/app/services/models/user.model';
+import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit, OnDestroy {
+  user!: User;
+  params!: Params;
+  private subscription: Subscription = new Subscription();
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(data => {this.params = data;})
+    this.apiService.getUsersID(this.params).subscribe(user => {
+      this.user = user;
+    });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
