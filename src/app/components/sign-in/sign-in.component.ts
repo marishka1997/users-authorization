03c1189aa696f88f835977bea12e99544805a41b
Mariam Gasparian <m.gasparian11@gmail.com>
2023-04-11 18:40:34 +0400

    feat: add business logic for SignInComponent

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit, OnDestroy {
+  signInForm!: FormGroup;
+  private subscription: Subscription = new Subscription();
 
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.signInForm = this.fb.group({
+      username: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+    });
+  }
+
+  signIn() {
+    const username = this.signInForm.get('username')?.value;
+    const password = this.signInForm.get('password')?.value;
+
+    if (username === 'admin' && password === 'admin') {
+      this.router.navigate(['./users']);
+    } else {
+      alert('Username or password is incorrect');
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
