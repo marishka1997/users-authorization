b2bd868c7f1b97bf3216b8f4c66c20eb7ebc739b
Mariam Gasparian <m.gasparian11@gmail.com>
2023-04-11 18:51:42 +0400

    fix: add business logic and change one method name for UsersComponent

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { User } from 'src/app/services/models/user.model';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, OnDestroy {
+  users: User[] = [];
+  subscription!: Subscription;
+  clicked = false;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.subscription = this.apiService
+      .getUsers()
+      .subscribe((users: User[]) => (this.users = users));
+  }
+
+  onClick() {
+    this.clicked = true;
+  }
+
+  removeUser(id: number) {
+    this.apiService.delete(id).subscribe(() => {
+      this.users = this.users.filter((user) => user.id !== id);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
