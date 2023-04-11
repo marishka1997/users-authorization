b5d8de0ec5ecac54e85df456f92345ff4ad83a92
Mariam Gasparian <m.gasparian11@gmail.com>
2023-04-11 18:48:02 +0400

    refactor: implements HttpClientModule and add methods for ApiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { User } from './models/user.model';
+import { HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Params } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+  }
+  getUsersID(id: Params): Observable<User> {
+    return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${id["id"]}`);
+  }
+  delete(id:number){
+    return this.http.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+  }
 }
